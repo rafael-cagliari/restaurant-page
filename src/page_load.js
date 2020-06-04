a18e2992603dfee3313ac397000e46f7650e2d2f
Rafael <rafael.cagliari22@gmail.com>
2020-06-04 19:39:34 -0300

    finished the menu tab and started info tab

--- src/page_load.js
+++ src/page_load.js
@@ -2,35 +2,68 @@ const pageLoad = () => {
     let div=document.querySelector('#content');
     let main = document.createElement('div');
     main.id = 'main';
-    main.classList.add('background')
+    main.classList.add('background');
     div.appendChild(main);
     const background = ()=>{
         let text = document.createElement('div');
-        text.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum quis lectus eu rhoncus. Vestibulum suscipit velit sapien, nec accumsan ante feugiat ac. Donec venenatis nunc consectetur mollis efficitur."
+        text.innerText = "The authentic Uruguayan Parrilla";
         text.classList.add('main-text');
-        text.id = 'main-text'
-        div.appendChild(text)
+        text.id = 'main-text';
+        div.appendChild(text);
     }
     return { background }
 }
 
 const home = () =>{
-    let main = document.querySelector("#main")
-    let text = document.querySelector("#main-text")
-    text.innerText = 'Home'
+    let main = document.querySelector("#main");
+    let text = document.querySelector("#main-text");
+    text.classList.remove('menu')
+    text.classList.add('main-text');
+    text.innerText = 'The authentic Uruguayan Parrilla';
 }
 
-
 const menu = () =>{
-    let main = document.querySelector("#main")
-    let text = document.querySelector("#main-text")
-    text.innerText = 'This is the menu!'
+    let main = document.querySelector("#main");
+    let text = document.querySelector("#main-text");
+    text.innerText = 'Menu';
+    text.classList.add('menu');
+    let container = document.createElement('div');
+    container.classList.add('menu-container');
+    text.appendChild(container);
+    let drinks = document.createElement('div');
+    let meals = document.createElement('div');
+    drinks.classList.add('menu-options');
+    meals.classList.add('menu-options');
+    container.appendChild(drinks);
+    container.appendChild(meals);
+    let menu_header_drinks = document.createElement('p');
+    let menu_content_drinks = document.createElement('p');
+    menu_header_drinks.innerText = 'Drinks'
+    menu_content_drinks.innerHTML = '<p>Cerveza Patricia <br>Tannat Tinto <br>Fernet <br>Caipirinha Brasilena <br>Whiskey<br>Soda<br>Water</p>'
+    menu_header_drinks.classList.add('menu-header');
+    menu_content_drinks.classList.add('menu-content');
+    drinks.appendChild(menu_header_drinks)
+    drinks.appendChild(menu_content_drinks);
+
+    let menu_header_meals = document.createElement('p');
+    let menu_content_meals = document.createElement('p');
+    menu_header_meals.innerText = 'Meals'
+    menu_content_meals.innerHTML = '<p>Bife de Chorizo <br>Ojo de Bife <br>Punta de Anca <br>Lomito de Cerdo<br>Churrasco</p>'
+    menu_header_meals.classList.add('menu-header');
+    menu_content_meals.classList.add('menu-content');
+    meals.appendChild(menu_header_meals)
+    meals.appendChild(menu_content_meals);
 }
 
 const info = () =>{
     let main = document.querySelector("#main")
     let text = document.querySelector("#main-text")
-    text.innerText = 'info'
+    text.classList.add('menu')
+    text.innerText = 'Info'
+    let info = document.createElement('p')
+    info.innerHTML = '<p> Founded by Don Ramon Sanchez in 1921 at the Porto region in Montevideu, the Parrillero is one of the most established Parrilla restaurants in the whole world according to critics and public alike.'
+    text.appendChild(info);
+    info.classList.add('info-text')
 }
 
 export { pageLoad, menu, info, home }
