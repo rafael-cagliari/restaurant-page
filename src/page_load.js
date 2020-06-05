d4384234fe0d8f79e59720ba03458387aaa292af
Rafael <rafael.cagliari22@gmail.com>
2020-06-05 17:41:40 -0300

    finished info tab and added media query

--- src/page_load.js
+++ src/page_load.js
@@ -17,7 +17,7 @@ const pageLoad = () => {
 const home = () =>{
     let main = document.querySelector("#main");
     let text = document.querySelector("#main-text");
-    text.classList.remove('menu')
+    text.classList.remove('menu','info')
     text.classList.add('main-text');
     text.innerText = 'The authentic Uruguayan Parrilla';
 }
@@ -26,6 +26,7 @@ const menu = () =>{
     let main = document.querySelector("#main");
     let text = document.querySelector("#main-text");
     text.innerText = 'Menu';
+    text.classList.remove('info');
     text.classList.add('menu');
     let container = document.createElement('div');
     container.classList.add('menu-container');
@@ -58,12 +59,16 @@ const menu = () =>{
 const info = () =>{
     let main = document.querySelector("#main")
     let text = document.querySelector("#main-text")
-    text.classList.add('menu')
+    text.classList.add('info')
     text.innerText = 'Info'
     let info = document.createElement('p')
     info.innerHTML = '<p> Founded by Don Ramon Sanchez in 1921 at the Porto region in Montevideu, the Parrillero is one of the most established Parrilla restaurants in the whole world according to critics and public alike.'
     text.appendChild(info);
     info.classList.add('info-text')
+    let location = document.createElement('p');
+    location.innerHTML = '<p style="margin-left:23px; text-align:left; text-shadow:none; font-size:2.2vw; font-style:italic">Location: Somewhere in Montevideu <br>Phone: 234 44312 28473<br>parrillero.restaurant@gmail.com</p>'
+    text.appendChild(location);
+
 }
 
 export { pageLoad, menu, info, home }
